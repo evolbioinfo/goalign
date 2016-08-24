3fdab3a902e56f6e8c16195ae1a9201a56aa724a
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2016-08-25 00:43:55 +0200

    Added bootstrap option for creating tar.gz

--- cmd/bootstrap.go
+++ cmd/bootstrap.go
@@ -15,6 +15,8 @@
 package cmd
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"fmt"
 	"github.com/fredericlemoine/goalign/io/fasta"
 	"github.com/fredericlemoine/goalign/io/phylip"
@@ -28,6 +30,7 @@ var bootstrapSeed int64
 var bootstrapNb int
 var bootstrapoutprefix string
 var bootstrapOrder bool
+var bootstraptargz bool
 
 // bootstrapCmd represents the bootstrap command
 var bootstrapCmd = &cobra.Command{
@@ -43,27 +46,74 @@ to quickly create a Cobra application.`,
 		if bootstrapoutprefix == "none" {
 			panic("Output bootstrap file prefix is mandatory")
 		}
+
 		rand.Seed(bootstrapSeed)
 		var f *os.File
 		var err error
-		for i := 0; i < bootstrapNb; i++ {
-			f, err = os.Create(bootstrapoutprefix + fmt.Sprintf("%d", i))
+		var tw *tar.Writer
+		var gw *gzip.Writer
+		var bootstring string
+
+		if bootstraptargz {
+			f, err = os.Create(bootstrapoutprefix + ".tar.gz")
 			if err != nil {
 				panic(err)
 			}
+			defer f.Close()
+			gw = gzip.NewWriter(f)
+			defer gw.Close()
+			tw = tar.NewWriter(gw)
+			defer tw.Close()
+		}
 
+		for i := 0; i < bootstrapNb; i++ {
+			bootid := bootstrapoutprefix + fmt.Sprintf("%d", i)
 			boot := rootalign.BuildBootstrap()
 			if bootstrapOrder {
 				boot.ShuffleSequences()
 			}
+
 			if rootphylip {
-				f.WriteString(phylip.WriteAlignment(boot))
+				bootstring = phylip.WriteAlignment(boot)
 			} else {
-				f.WriteString(fasta.WriteAlignment(boot))
+				bootstring = fasta.WriteAlignment(boot)
 			}
-			f.Close()
+
+			if bootstraptargz {
+				if err = addstringtotargz(tw, bootid, bootstring); err != nil {
+					panic(err)
+				}
+			} else {
+				f, err = os.Create(bootid)
+				if err != nil {
+					panic(err)
+				}
+				f.WriteString(bootstring)
+				f.Close()
+			}
 		}
 	},
+}
+
+func addstringtotargz(tw *tar.Writer, name string, align string) error {
+	// now lets create the header as needed for this file within the tarball
+	alignbytes := []byte(align)
+
+	header := new(tar.Header)
+	header.Name = name
+	header.Size = int64(len(alignbytes))
+	header.Mode = 436 //int64(stat.Mode())
+	header.ModTime = time.Now()
+	// write the header to the tarball archive
+	if err := tw.WriteHeader(header); err != nil {
+		return err
+	}
+
+	// copy the file data to the tarball
+	if _, err := tw.Write(alignbytes); err != nil {
+		return err
+	}
+	return nil
 }
 
 func init() {
@@ -71,6 +121,7 @@ func init() {
 
 	bootstrapCmd.PersistentFlags().Int64VarP(&bootstrapSeed, "seed", "s", time.Now().UTC().UnixNano(), "Initial Random Seed")
 	bootstrapCmd.PersistentFlags().BoolVarP(&bootstrapOrder, "shuf-order", "S", false, "Also shuffle order of sequences in bootstrap files")
+	bootstrapCmd.PersistentFlags().BoolVar(&bootstraptargz, "tar-gz", false, "Will create a single targz file with all bootstrap alignments")
 	bootstrapCmd.PersistentFlags().IntVarP(&bootstrapNb, "nboot", "n", 1, "Prefix of output bootstrap files")
 	bootstrapCmd.PersistentFlags().StringVarP(&bootstrapoutprefix, "prefix", "r", "none", "Number of bootstrap replicates to build")
 }
