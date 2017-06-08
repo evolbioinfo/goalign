9657831982cc942707156fc9a7ae40cce6be1c3a
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2017-06-08 17:21:25 +0200

    alleles docs

--- cmd/alleles.go
+++ cmd/alleles.go
@@ -23,13 +23,8 @@ import (
 // allelesCmd represents the alleles command
 var allelesCmd = &cobra.Command{
 	Use:   "alleles",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Prints the average number of alleles per sites of the alignment",
+	Long:  `Prints the average number of alleles per sites of the alignment.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		for al := range rootaligns {
 			fmt.Println(al.AvgAllelesPerSite())
