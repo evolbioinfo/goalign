b2f77983dde4a3035532a9b47d476c0b2777a54e
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2018-09-20 11:52:52 +0200

    Changed trim seq command line option --from-start,-s to --from-left,-l

--- cmd/seq.go
+++ cmd/seq.go
@@ -5,7 +5,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var trimFromStart bool
+var trimFromLeft bool
 
 // seqCmd represents the seq command
 var seqCmd = &cobra.Command{
@@ -13,16 +13,17 @@ var seqCmd = &cobra.Command{
 	Short: "Trims sequences of the alignment",
 	Long: `Trims sequences of the alignemnt
 
-It trims n (--nb-char, -n) characters from the beginning (--from-start, -s) or from the end (default) of the input alignment.
+It trims n (--nb-char, -n) characters from the beginning (--from-left, -l) or from the end (default) of the input alignment.
 
 Example:
 goalign trim seq -i align.fa -o trimed.fa -s -n 10
 
 `,
 	Run: func(cmd *cobra.Command, args []string) {
+		aligns := readalign(infile)
 		f := openWriteFile(trimAlignOut)
-		for al := range rootaligns.Achan {
-			if err := al.TrimSequences(trimNb, trimFromStart); err != nil {
+		for al := range aligns.Achan {
+			if err := al.TrimSequences(trimNb, trimFromLeft); err != nil {
 				io.ExitWithMessage(err)
 			} else {
 				writeAlign(al, f)
@@ -35,5 +36,5 @@ goalign trim seq -i align.fa -o trimed.fa -s -n 10
 func init() {
 	trimCmd.AddCommand(seqCmd)
 	seqCmd.PersistentFlags().IntVarP(&trimNb, "nb-char", "n", 1, "Number of characters to trim from sequences")
-	seqCmd.PersistentFlags().BoolVarP(&trimFromStart, "from-start", "s", false, "If true: trims n char from start, else from end")
+	seqCmd.PersistentFlags().BoolVarP(&trimFromLeft, "from-left", "l", false, "If true: trims n char from the left, else from the right")
 }
