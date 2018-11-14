c338fd68ab328d2bf6431d66aebbf3fbf3d2ee14
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2018-11-14 09:15:19 +0100

    Added length command for unaligned sequences

--- cmd/length.go
+++ cmd/length.go
@@ -15,7 +15,11 @@ var lengthCmd = &cobra.Command{
 	Long: `Prints the length of sequences in the alignment. 
 May take a Phylip of Fasta input alignment.
 
+If --unaligned is given, then length of all individual sequences is printed.
+
 If the input alignment contains several alignments, will take all of them
+
+
 
 Example of usages:
 
@@ -24,25 +28,39 @@ goalign stats length -i align.fasta
 
 `,
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
-		var aligns align.AlignChannel
+		if unaligned {
+			var seqs align.SeqBag
 
-		if aligns, err = readalign(infile); err != nil {
-			io.LogError(err)
-			return
-		}
+			if seqs, err = readsequences(infile); err != nil {
+				io.LogError(err)
+				return
+			}
 
-		for al := range aligns.Achan {
-			fmt.Println(al.Length())
-		}
+			seqs.IterateChar(func(name string, sequence []rune) {
+				fmt.Println(name, "\t", len(sequence))
+			})
+		} else {
+			var aligns align.AlignChannel
 
-		if aligns.Err != nil {
-			err = aligns.Err
-			io.LogError(err)
+			if aligns, err = readalign(infile); err != nil {
+				io.LogError(err)
+				return
+			}
+
+			for al := range aligns.Achan {
+				fmt.Println(al.Length())
+			}
+
+			if aligns.Err != nil {
+				err = aligns.Err
+				io.LogError(err)
+			}
 		}
 		return
 	},
 }
 
 func init() {
+	statsCmd.PersistentFlags().BoolVar(&unaligned, "unaligned", false, "Considers sequences as unaligned and format fasta (phylip, nexus,... options are ignored)")
 	statsCmd.AddCommand(lengthCmd)
 }
