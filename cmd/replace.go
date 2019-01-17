9a0d92ef4f3b39f9123ffd245524a7a03c84f58f
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2019-01-17 15:42:00 +0100

    Added cmd/replace.go

---
+++ cmd/replace.go
@@ -0,0 +1,72 @@
+package cmd
+
+import (
+	"errors"
+	"os"
+
+	"github.com/fredericlemoine/goalign/align"
+	"github.com/fredericlemoine/goalign/io"
+	"github.com/spf13/cobra"
+)
+
+var replaceOutput string
+var replaceRegexp bool
+var replaceOld, replaceNew string
+
+// renameCmd represents the rename command
+var replaceCmd = &cobra.Command{
+	Use:   "replace",
+	Short: "Replace characters in sequences of the input alignment (possible with a regex)",
+	Long: `Replace characters in sequences of the input alignment (possible with a regex).
+
+`,
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		var aligns align.AlignChannel
+		var f *os.File
+		var seqs align.SeqBag
+
+		if !cmd.Flags().Changed("old") || !cmd.Flags().Changed("old") {
+			err = errors.New("--old and --new must be specified")
+			return
+		}
+
+		if f, err = openWriteFile(replaceOutput); err != nil {
+			io.LogError(err)
+			return
+		}
+		defer closeWriteFile(f, replaceOutput)
+
+		if unaligned {
+			if seqs, err = readsequences(infile); err != nil {
+				io.LogError(err)
+				return
+			}
+			seqs.Replace(replaceOld, replaceNew, replaceRegexp)
+			writeSequences(seqs, f)
+		} else {
+			if aligns, err = readalign(infile); err != nil {
+				io.LogError(err)
+				return
+			}
+			for al := range aligns.Achan {
+				al.Replace(replaceOld, replaceNew, replaceRegexp)
+				writeAlign(al, f)
+			}
+			if aligns.Err != nil {
+				err = aligns.Err
+				io.LogError(err)
+			}
+		}
+		return
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(replaceCmd)
+
+	replaceCmd.PersistentFlags().StringVarP(&replaceOutput, "output", "o", "stdout", "Output alignment file")
+	replaceCmd.PersistentFlags().BoolVarP(&replaceRegexp, "regexp", "e", false, "Considers Replace alignment using regexp")
+	replaceCmd.PersistentFlags().StringVarP(&replaceOld, "old", "s", "none", "String to replace in the sequences")
+	replaceCmd.PersistentFlags().StringVarP(&replaceNew, "new", "n", "none", "New string that will replace old string in sequences")
+	replaceCmd.PersistentFlags().BoolVar(&unaligned, "unaligned", false, "Considers input sequences as unaligned and fasta format (phylip, nexus,... options are ignored)")
+}
