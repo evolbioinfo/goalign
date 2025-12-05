b6cf30efa49c016020617dd21af83c614dd5e115
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2025-12-05 16:20:47 +0100

    Added image generation for simplot

--- cmd/computesimplot.go
+++ cmd/computesimplot.go
@@ -2,8 +2,15 @@ package cmd
 
 import (
 	"fmt"
+	"image/color"
 
 	"github.com/spf13/cobra"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/font"
+	"gonum.org/v1/plot/palette/brewer"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
 
 	"github.com/evolbioinfo/goalign/align"
 	"github.com/evolbioinfo/goalign/distance/dna"
@@ -15,6 +22,9 @@ var simplotOutput string
 var simplotModel string
 var simplotRefSeq string
 var simplotWindows int
+var simplotOutImageFile string
+var simplotOutImageWidth int
+var simplotOutImageHeight int
 
 // computedistCmd represents the computedist command
 var computeSimplotCmd = &cobra.Command{
@@ -26,6 +36,10 @@ var computeSimplotCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
 		var f utils.StringWriterCloser
 		var aligns *align.AlignChannel
+		var p *plot.Plot
+		var point *plotter.Scatter
+		var line *plotter.Line
+
 		var windows []struct {
 			WindowStart, WindowEnd int
 			CompSeq                string
@@ -58,6 +72,56 @@ var computeSimplotCmd = &cobra.Command{
 			for _, w := range windows {
 				fmt.Fprintf(f, "%d\t%d\t%s\t%v\n", w.WindowStart, w.WindowEnd, w.CompSeq, w.Distance)
 			}
+
+			if simplotOutImageFile != "none" {
+				p = plot.New()
+				p.Title.Text = "Simplot"
+				p.X.Label.Text = "Genomic position"
+				p.Y.Label.Text = "Similarity to reference"
+
+				colors := brewer.DivergingPalette{
+					ID:         "div",
+					Name:       "div",
+					Laptop:     brewer.Good,
+					CRT:        brewer.Good,
+					ColorBlind: brewer.Good,
+					Copy:       brewer.Good,
+					Projector:  brewer.Good,
+					Color: []color.Color{
+						color.RGBA{R: 212, G: 222, B: 69, A: 255},
+						color.RGBA{R: 116, G: 209, B: 94, A: 255},
+						color.RGBA{R: 67, G: 89, B: 186, A: 255},
+						color.RGBA{R: 199, G: 76, B: 184, A: 255},
+						color.RGBA{R: 247, G: 184, B: 13, A: 255},
+					},
+				}
+
+				pts := make(map[string]plotter.XYs)
+				for _, l := range windows {
+					xy := plotter.XY{X: float64(l.WindowStart), Y: 1.0 - l.Distance}
+					pts[l.CompSeq] = append(pts[l.CompSeq], xy)
+				}
+
+				i := 0
+				t := plotter.PaletteThumbnailers(colors)
+				for k := range pts {
+					line, point, err = plotter.NewLinePoints(pts[k])
+					point.Shape = draw.CircleGlyph{}
+					point.Radius = 1
+					point.Color = colors.Colors()[i%len(colors.Colors())]
+					line.Color = colors.Colors()[i%len(colors.Colors())]
+					line.Width = 2
+					p.Add(line, point)
+					p.Legend.Add(k, t[i%len(colors.Colors())])
+					i++
+				}
+				// Save the plot to a file.
+				if err = p.Save(font.Length(simplotOutImageWidth)*vg.Inch, font.Length(simplotOutImageHeight)*vg.Inch, simplotOutImageFile); err != nil {
+					io.LogError(err)
+					return
+				}
+			}
+
 		}
 		return
 	},
@@ -69,4 +133,7 @@ func init() {
 	computeSimplotCmd.PersistentFlags().StringVarP(&simplotModel, "model", "m", "k2p", "Model for distance computation")
 	computeSimplotCmd.PersistentFlags().StringVarP(&simplotRefSeq, "refseq", "r", "-", "Reference sequence to compare all others")
 	computeSimplotCmd.PersistentFlags().IntVarP(&simplotWindows, "window-size", "w", 100, "Window size")
+	computeSimplotCmd.PersistentFlags().StringVar(&simplotOutImageFile, "image", "none", "LTT plot image image output file")
+	computeSimplotCmd.PersistentFlags().IntVar(&simplotOutImageWidth, "image-width", 4, "LTT plot image image output width")
+	computeSimplotCmd.PersistentFlags().IntVar(&simplotOutImageHeight, "image-height", 4, "LTT plot image output heigh")
 }
--- go.mod
+++ go.mod
@@ -8,21 +8,30 @@ require (
 	github.com/spf13/cobra v1.5.0
 	github.com/ulikunitz/xz v0.5.10
 	gonum.org/v1/gonum v0.9.3
+	gonum.org/v1/plot v0.9.0
 )
 
 require (
 	github.com/abiosoft/ishell v2.0.0+incompatible // indirect
 	github.com/abiosoft/readline v0.0.0-20180607040430-155bce2042db // indirect
+	github.com/ajstarks/svgo v0.0.0-20180226025133-644b8db467af // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/fatih/color v1.7.0 // indirect
 	github.com/flynn-archive/go-shlex v0.0.0-20150515145356-3f9db97f8568 // indirect
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/go-fonts/liberation v0.1.1 // indirect
+	github.com/go-latex/latex v0.0.0-20210118124228-b3d85cf34e07 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/mattn/go-colorable v0.0.9 // indirect
 	github.com/mattn/go-isatty v0.0.3 // indirect
+	github.com/phpdave11/gofpdf v1.4.2 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/exp v0.0.0-20200224162631-6cc2880d07d6 // indirect
+	golang.org/x/image v0.0.0-20210216034530-4410531fe030 // indirect
 	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.3.5 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
